de707cdbaa985a49a84508986296a52047351cce
vedant_powar <vedantpowar39@gmail.com>
2026-02-13 11:26:44 +0530

    Update python_calculator.py

--- python_calculator.py
+++ python_calculator.py
@@ -1,35 +1,3 @@
-# # Simple Calculator
-
-# num1 = float(input("Enter first number: "))
-# num2 = float(input("Enter second number: "))
-
-# print("Choose operation:")
-# print("1. Addition (+)")
-# print("2. Subtraction (-)")
-# print("3. Multiplication (*)")
-# print("4. Division (/)")
-
-# choice = input("Enter choice (1/2/3/4): ")
-
-# if choice == '1':
-#     print("Result:", num1 + num2)
-
-# elif choice == '2':
-#     print("Result:", num1 - num2)
-
-# elif choice == '3':
-#     print("Result:", num1 * num2)
-
-# elif choice == '4':
-#     if num2 != 0:
-#         print("Result:", num1 / num2)
-#     else:
-#         print("Error! Division by zero.")
-
-# else:
-#     print("Invalid choice")
-
-
 import tkinter as tk
 
 def button_click(value):
@@ -81,4 +49,5 @@ for i in range(4):
     root.grid_columnconfigure(i, weight=1)
 
 root.mainloop()
+
 
